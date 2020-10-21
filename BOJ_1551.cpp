e69cb9f49136ada4a41decf0bdd8ba9ec2ea202a
hyogeun <yhg0219@gmail.com>
2020-10-21 23:09:37 +0900

    BOJ_1551 수열의 변화

---
+++ BOJ_1551.cpp
@@ -0,0 +1,25 @@
+// BOJ_1551 수열의 변화
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+float max(float a, float b){
+    return a > b ? a : b;
+}
+
+int round(float input){
+    float temp = (int)input;
+    return input != temp ? input + 1 : input;
+}
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    freopen("../input.txt", "r", stdin);
+
+    string input;
+
+    return 0;
+}
